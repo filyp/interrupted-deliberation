0259ebba010f72a80e8a7ee5e43d19cb022b4556
Filip Sondej <26285707+filyp@users.noreply.github.com>
2025-05-12 17:16:01 +0200

    interrupts and getting accuracy

--- playground.py
+++ playground.py
@@ -23,13 +23,16 @@ model = AutoModelForCausalLM.from_pretrained(
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 tokenizer.pad_token = tokenizer.eos_token
 
+answer_tokens = [" A", " B", " C", " D"]
+answer_ids = pt.tensor([tokenizer.encode(t)[1:] for t in answer_tokens]).reshape(4)
+
 # %%
 dataset_name = "maveriq/bigbenchhard"
 # subset = "web_of_lies"
 subset = "logical_deduction_three_objects"
 dataset = load_dataset(dataset_name, subset, split="train")
 
-question = dataset[0]
+question = dataset[1]
 
 # %%
 full_prompt = f"""\
@@ -38,16 +41,43 @@ full_prompt = f"""\
 Reason about the question step by step.
 
 At the end output:
-ANSWER=X
+ANSWER: X
 
 Reasoning:
 """
-print(full_prompt)
+original_batch = tokenizer(full_prompt, return_tensors="pt")
+
+pt.manual_seed(42)
+# generate original CoT
+original_out = model.generate(**original_batch, max_new_tokens=1000, temperature=1.0)
+
+# print(tokenizer.decode(original_out[0]))
+# print(question["target"])
 
 # %%
-batch = tokenizer(full_prompt, return_tensors="pt")
-out = model.generate(**batch, max_new_tokens=500)
+def get_accuracy(out, question):
+    last_token_logits = out.logits[0, -1]
+    probs = pt.softmax(last_token_logits, dim=-1)
 
-out_text = tokenizer.decode(out[0])
-print(out_text)
+    answer_probs = probs[answer_ids]
+    assert answer_probs.sum() > 0.8  # if it's smaller, we're prompting incorrectly
+
+    answer_probs /= answer_probs.sum()  # normalize
+
+    _targets = ["(A)", "(B)", "(C)", "(D)", "(E)", "(F)", "(G)"]
+    target_idx = _targets.index(question["target"])
+
+    return answer_probs[target_idx]
+
+
+interrupt_prompt = "\n\nANSWER:"
+
+out_text_trimmed = tokenizer.decode(original_out[0, :-180])
+print(out_text_trimmed + interrupt_prompt)
+batch = tokenizer(out_text_trimmed + interrupt_prompt, return_tensors="pt")
+
+pt.cuda.empty_cache()
+out = model(**batch)
+get_accuracy(out, question)
+
 # %%
